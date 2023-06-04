ae7b1c2d652272b711bd69d4f0914ec88a1b4a6d
Daniel Santos <dsantosgames20@gmail.com>
2023-06-04 13:06:28 -0300

    70%

--- script.js
+++ script.js
@@ -11,3 +11,12 @@ function ativarLetra(elemento) {
 const titulo = document.querySelector('.digitando')
 ativarLetra(titulo)
 
+
+const ativaMenu = document.querySelector('.fa-bars')
+const navMenu = document.querySelector('header .navegacao-primaria')
+
+ativaMenu.addEventListener('click', ()=>{
+    ativaMenu.classList.toggle('fa-x')
+    navMenu.classList.toggle('ativado')
+})
+
